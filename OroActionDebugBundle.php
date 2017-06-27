788c01a61b7854960a2d3ab109f6fe8dd8a2bf7a
Yauheni Kasenka <ykasenka@oroinc.com>
2017-06-27 14:10:57 +0300

    - Created Initial Bundle structure

---
+++ OroActionDebugBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Oro\Bundle\ActionDebugBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OroActionDebugBundle extends Bundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        // nothing
+    }
+}
